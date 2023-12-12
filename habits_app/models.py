46fa5cfe2807dd18b83902684635d5412e83ab9c
TagRomEdu <qwertystyles@gmail.com>
2023-12-12 15:25:24 +0500

    created model habit

--- habits_app/models.py
+++ habits_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from config import settings
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Habit(models.Model):
+    PERIOD_HOURLY = 'hourly'
+    PERIOD_DAILY = 'daily'
+    PERIOD_WEEKLY = 'weekly'
+
+    PERIODS = (
+        (PERIOD_HOURLY, 'Ежечасно'),
+        (PERIOD_DAILY, 'Ежедневно'),
+        (PERIOD_WEEKLY, 'Еженедельно')
+    )
+
+    owner = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    place = models.CharField(_("place"), max_length=200)
+    time = models.TimeField(_("time"))
+    activity = models.TextField(_("activity"))
+    pleasant_habit = models.ForeignKey(to='self', on_delete=models.CASCADE,  **NULLABLE, related_name='pleasant_set')
+    related_habit = models.ForeignKey(to='self', on_delete=models.CASCADE, **NULLABLE, related_name='related_set')
+    period = models.CharField(_("period"), max_length=50, choices=PERIODS, default=PERIOD_DAILY)
+    reward = models.TextField(_("reward"), **NULLABLE)
+    time_required = models.PositiveSmallIntegerField(_("time_required"), default=5)
+    is_published = models.BooleanField(_("published"), default=False)
+
+    def __str__(self):
+        return f'{self.pk} - {self.activity}'
+
+    class Meta:
+        verbose_name = "Привычка"
+        verbose_name_plural = "Привычки"
