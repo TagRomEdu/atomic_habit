145a9bad52902bf532c2db8f1c85b0d6386a5286
TagRomEdu <qwertystyles@gmail.com>
2023-12-12 13:30:58 +0500

    create command for su

---
+++ users_app/management/commands/create_su.py
@@ -0,0 +1,17 @@
+from django.core.management import BaseCommand
+
+from users_app.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='m@sster.ru',
+            first_name='Romka',
+            last_name='T',
+            is_staff=True,
+            is_superuser=True
+        )
+
+        user.set_password('Aa12345!')
+        user.save()
