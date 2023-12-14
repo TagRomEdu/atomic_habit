8841aef5d4603a88269e4a1e163a688666fdb0f6
TagRomEdu <qwertystyles@gmail.com>
2023-12-14 15:31:31 +0500

    added views for RUD

--- habits_app/views.py
+++ habits_app/views.py
@@ -1,7 +1,27 @@
 from rest_framework import generics
 
+from habits_app.models import Habit
 from habits_app.serializers import HabitSerializer
 
 
 class HabitCreateAPIView(generics.CreateAPIView):
     serializer_class = HabitSerializer
+
+
+class HabitListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitDestroyAPIView(generics.DestroyAPIView):
+    queryset = Habit.objects.all()
