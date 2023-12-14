9f623f472897dace30ade7f48bc5329803c26cac
TagRomEdu <qwertystyles@gmail.com>
2023-12-14 15:40:23 +0500

    added urls

--- habits_app/urls.py
+++ habits_app/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
 from habits_app.apps import HabitsAppConfig
-from habits_app.views import HabitCreateAPIView
-
+from habits_app.views import HabitCreateAPIView, HabitListAPIView, HabitRetrieveAPIView, HabitUpdateAPIView, \
+    HabitDestroyAPIView
 
 app_name = HabitsAppConfig.name
 
 urlpatterns = [
-    path('create/', HabitCreateAPIView.as_view(), name='create_habit')
+    path('create/', HabitCreateAPIView.as_view(), name='create_habit'),
+    path('list/', HabitListAPIView.as_view(), name='list_habit'),
+    path('retrieve/<int:pk>', HabitRetrieveAPIView.as_view(), name='retrieve_habit'),
+    path('update/<int:pk>', HabitUpdateAPIView.as_view(), name='update_habit'),
+    path('destroy/<int:pk>', HabitDestroyAPIView.as_view(), name='destroy_habit'),
 ]
