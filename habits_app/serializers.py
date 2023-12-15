a5fee7ee0ee1a8f2665723f20f56483840267c25
TagRomEdu <qwertystyles@gmail.com>
2023-12-15 12:16:17 +0500

    added serializer for checking requiring time

--- habits_app/serializers.py
+++ habits_app/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 
 from habits_app.models import Habit
-from habits_app.validators import RewardPleasantValidator
+from habits_app.validators import RewardPleasantValidator, TimeValidator
 
 
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = '__all__'
-        validators = [RewardPleasantValidator(field_1='related_habit', field_2='reward', field_3='is_pleasant')]
+        validators = [RewardPleasantValidator(field_1='related_habit', field_2='reward', field_3='is_pleasant'),
+                      TimeValidator(field_1='time_required')]
--- habits_app/validators.py
+++ habits_app/validators.py
@@ -22,48 +22,10 @@ class RewardPleasantValidator:
 
 
 class TimeValidator:
-    def __init__(self, field_1, field_2):
+    def __init__(self, field_1):
         self.field_1 = field_1
-        self.field_2 = field_2
 
     def __call__(self, value):
-        related_habit = dict(value).get(self.field_1)
-        reward = dict(value).get(self.field_2)
-        if related_habit and reward:
-            raise ValidationError("У привычки, имеющей связанную привычку, не может быть награды.")
-
-
-class PleasantValidator:
-    def __init__(self, field_1, field_2):
-        self.field_1 = field_1
-        self.field_2 = field_2
-
-    def __call__(self, value):
-        related_habit = dict(value).get(self.field_1)
-        reward = dict(value).get(self.field_2)
-        if related_habit and reward:
-            raise ValidationError("У привычки, имеющей связанную привычку, не может быть награды.")
-
-
-class RewardValidator:
-    def __init__(self, field_1, field_2):
-        self.field_1 = field_1
-        self.field_2 = field_2
-
-    def __call__(self, value):
-        related_habit = dict(value).get(self.field_1)
-        reward = dict(value).get(self.field_2)
-        if related_habit and reward:
-            raise ValidationError("У привычки, имеющей связанную привычку, не может быть награды.")
-
-
-class RewardValidator:
-    def __init__(self, field_1, field_2):
-        self.field_1 = field_1
-        self.field_2 = field_2
-
-    def __call__(self, value):
-        related_habit = dict(value).get(self.field_1)
-        reward = dict(value).get(self.field_2)
-        if related_habit and reward:
-            raise ValidationError("У привычки, имеющей связанную привычку, не может быть награды.")
+        time = dict(value).get(self.field_1)
+        if int(time) > 120:
+            raise ValidationError("Время выполнения привычки не должно быть более 120 секунд, капуша!")
