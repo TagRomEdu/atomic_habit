99f78eb7da6b3093da3117cc6350acd2a0744c65
TagRomEdu <qwertystyles@gmail.com>
2023-12-27 11:43:08 +0500

    added celery settings

--- config/settings.py
+++ config/settings.py
@@ -165,18 +165,14 @@ SWAGGER_SETTINGS = {
     },
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379'  # Например, Redis, который по умолчанию работает на порту 6379
+CELERY_BROKER_URL = 'redis://localhost:6379' 
 
-# URL-адрес брокера результатов, также Redis
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 
-# Часовой пояс для работы Celery
 CELERY_TIMEZONE = "Australia/Tasmania"
 
-# Флаг отслеживания выполнения задач
 CELERY_TASK_TRACK_STARTED = True
 
-# Максимальное время на выполнение задачи
 CELERY_TASK_TIME_LIMIT = 1 * 60
 
 EMAIL_HOST = 'smtp.yandex.ru'
