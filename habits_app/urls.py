1d5b176d7526cc8951cd0656121a3efe985fb732
TagRomEdu <qwertystyles@gmail.com>
2023-12-27 10:44:55 +0500

    created view for public habits

--- habits_app/urls.py
+++ habits_app/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 
 from habits_app.apps import HabitsAppConfig
 from habits_app.views import HabitCreateAPIView, HabitListAPIView, HabitRetrieveAPIView, HabitUpdateAPIView, \
-    HabitDestroyAPIView
+    HabitDestroyAPIView, HabitPublishListAPIView
 
 app_name = HabitsAppConfig.name
 
 urlpatterns = [
     path('create/', HabitCreateAPIView.as_view(), name='create_habit'),
     path('list/', HabitListAPIView.as_view(), name='list_habit'),
+    path('list/publish/', HabitPublishListAPIView.as_view(), name='publish_list_habit'),
     path('retrieve/<int:pk>', HabitRetrieveAPIView.as_view(), name='retrieve_habit'),
     path('update/<int:pk>', HabitUpdateAPIView.as_view(), name='update_habit'),
     path('destroy/<int:pk>', HabitDestroyAPIView.as_view(), name='destroy_habit'),
--- habits_app/views.py
+++ habits_app/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 
 from habits_app.models import Habit
 from habits_app.paginators import HabitPaginator
+from habits_app.permissions import IsOwner
 from habits_app.serializers import HabitSerializer
 
 
@@ -13,17 +14,29 @@ class HabitListAPIView(generics.ListAPIView):
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
     pagination_class = HabitPaginator
+    
+    def get_queryset(self):
+        return Habit.objects.filter(owner=self.request.user)
 
 
+class HabitPublishListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.filter(is_published=True)
+    pagination_class = HabitPaginator
+
+    
 class HabitRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
+    permission_classes = [IsOwner]
 
 
 class HabitUpdateAPIView(generics.UpdateAPIView):
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
+    permission_classes = [IsOwner]
 
 
 class HabitDestroyAPIView(generics.DestroyAPIView):
     queryset = Habit.objects.all()
+    permission_classes = [IsOwner]
